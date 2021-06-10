aed7ebbefd4a93ef7d793c48ba255717dcd666ac
Diego <lima.j.diego@gmail.com>
2021-06-10 11:05:30 -0300

    Definindo conexão com host

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ def main():
         sys.exit()
 
     print('Socket criado com succeso')
+
+
+    # Definir o host que vai ser conectado
+
+    hostuser = input('Digite o host ou ip a ser conectado: ')
+    portuser = input('Digite a porta a ser conectada: ')
+
+    try:
+        s.connect((hostuser, int(portuser)))
+        print(f'Cliente TCP Conectado com sucesso no Host: {hostuser}, na porta: {portuser}')
+        s.shutdown(2)  # Espera dois segundos para finalizar a execução
+    except socket.error as error:
+        print(f'Não foi possível se conectar ao Host. Erro: {error}')
+        sys.exit()
+
+
+main()
