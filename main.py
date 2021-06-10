6b70122ec31f991b86b04eccbfd54d16842f590a
Diego <lima.j.diego@gmail.com>
2021-06-10 10:58:45 -0300

    Definindo função para tentar um conexão tcp/ip e criar um socket

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import socket
 import sys
 
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as error:
+        print(f'Falha na conexão! Error: {error}')
+        sys.exit()
+
+    print('Socket criado com succeso')
